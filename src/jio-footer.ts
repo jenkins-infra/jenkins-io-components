24c55326fd0641d4b1f7980d8ac7f88838e5e924
Gavin Mogan <git@gavinmogan.com>
2022-10-16 16:57:46 -0700

    chore: fix lint issue

--- src/jio-footer.ts
+++ src/jio-footer.ts
@@ -1,4 +1,4 @@
-import {LitElement, html, css, TemplateResult} from 'lit';
+import {LitElement, html, TemplateResult} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
 import {relOrAbsoluteLink} from './jio-navbar-link';
