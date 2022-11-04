d771b7513570ba073d224ce43508aec2c2b2262e
Gavin Mogan <git@gavinmogan.com>
2022-11-03 21:59:31 -0700

    fix: pass github branch to report-a-problem too

--- src/test/jio-footer_test.ts
+++ src/test/jio-footer_test.ts
@@ -1,4 +1,6 @@
 import {Footer} from '../jio-footer.js';
+import {ImproveThisPage} from '../jio-improve-this-page.js';
+import {ReportAProblem} from '../jio-report-a-problem.js';
 
 import {fixture, assert} from '@open-wc/testing';
 import {html} from 'lit/static-html.js';
@@ -16,8 +18,20 @@ suite('jio-footer', () => {
       return assert.isAccessible(el);
     });
   });
+  test('properties', async () => {
+    const el = (await fixture(html`<jio-footer githubBranch="master" githubRepo="jenkins-infra/jenkins.io" property="https://www.jenkins.io" sourcePath="index.html.haml"></jio-footer>`)) as Footer;
+    assert.instanceOf(el, Footer);
+    assert.shadowDom.notEqual(el, ``);
+
+    assert.equal(el.githubBranch, 'master');
+    const improveThisPage = el.shadowRoot?.querySelector('jio-improve-this-page') as ImproveThisPage;
+    assert.equal(improveThisPage.githubBranch, 'master');
+    assert.equal(improveThisPage.githubRepo, 'jenkins-infra/jenkins.io');
+    assert.equal(improveThisPage.sourcePath, 'index.html.haml');
+
+    const reportAProblem = el.shadowRoot?.querySelector('jio-report-a-problem') as ReportAProblem;
+    assert.equal(reportAProblem.githubBranch, 'master');
+    assert.equal(reportAProblem.githubRepo, 'jenkins-infra/jenkins.io');
+    assert.equal(reportAProblem.sourcePath, 'index.html.haml');
+  });
 });
-
-
-
-
