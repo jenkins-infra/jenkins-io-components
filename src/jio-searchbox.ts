9ec9179c1bef8de402fb0733ec748891da0b790c
Gavin Mogan <git@gavinmogan.com>
2022-11-21 23:56:08 -0800

    fix: lazy load algoila library as needed

--- src/jio-searchbox.ts
+++ src/jio-searchbox.ts
@@ -1,6 +1,5 @@
-import {LitElement, html, css} from 'lit';
+import {LitElement, css} from 'lit';
 import {customElement} from 'lit/decorators.js';
-import {ref, createRef} from 'lit/directives/ref.js';
 
 declare global {
   interface Window {
@@ -21,45 +20,27 @@ export class Searchbox extends LitElement {
 
   override connectedCallback() {
     super.connectedCallback();
-    if (!window.docsearch) {
-      // have to add css to both the page (for popup) and webcomponent (button)
-      [document.head, this.renderRoot].forEach(root => {
-        const linkFileEl = document.createElement('link');
-        linkFileEl.setAttribute('rel', 'stylesheet');
-        linkFileEl.setAttribute('type', 'text/css');
-        linkFileEl.setAttribute('href', `https://cdn.jsdelivr.net/npm/@docsearch/css@3`);
-        linkFileEl.setAttribute('media', 'all');
-        root.appendChild(linkFileEl);
-      });
-
-      const scriptFileEl = document.createElement('script');
-      scriptFileEl.setAttribute('defer', '');
-      scriptFileEl.setAttribute('src', `https://cdn.jsdelivr.net/npm/@docsearch/js@3`);
-      document.head.appendChild(scriptFileEl);
-      scriptFileEl.addEventListener('load', this.onAlgoliaReady.bind(this));
-    } else {
-      setTimeout(this.onAlgoliaReady.bind(this), 0);
-    }
-  }
-
-  private onAlgoliaReady() {
-    window.docsearch({
-      container: this.searchRef.value!,
-      indexName: 'jenkins',
-      appId: "M6L7Q4Z8HS",
-      apiKey: "52f8dfbff76ffd9106f1c68fee16154b",
-      searchParameters: {
-        facetFilters: ["lang:en"],
-      },
-      debug: false
+    // have to add css to both the page (for popup) and webcomponent (button)
+    [document.head, this.renderRoot].forEach(root => {
+      const linkFileEl = document.createElement('link');
+      linkFileEl.setAttribute('rel', 'stylesheet');
+      linkFileEl.setAttribute('type', 'text/css');
+      linkFileEl.setAttribute('href', `https://cdn.jsdelivr.net/npm/@docsearch/css@3`);
+      linkFileEl.setAttribute('media', 'all');
+      root.appendChild(linkFileEl);
     });
-    this._isReady = true;
-  }
-
-  private searchRef = createRef();
-
-  override render() {
-    return html`<div data-testid="searchbox"><div ${ref(this.searchRef)}></div></div>`;
+    import('@docsearch/js').then(({default: docsearch}) => {
+      docsearch({
+        container: this.renderRoot as HTMLElement,
+        indexName: 'jenkins',
+        appId: "M6L7Q4Z8HS",
+        apiKey: "52f8dfbff76ffd9106f1c68fee16154b",
+        searchParameters: {
+          facetFilters: ["lang:en"],
+        },
+      });
+      this._isReady = true;
+    });
   }
 }
 
