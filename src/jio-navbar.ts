128d02405b20933d671c018fd70cefd173a5cee2
Alexander Brandes <mc.cache@web.de>
2023-02-24 19:23:38 +0100

    feat: Remove references to sunset chinese website (#70)
    The chinese website remains almost unmodified since 2019 and there are
    ongoing efforts to detach it from our resources.
    I would like to remove it from the navbar component. Not only for the
    reason that it's in a sunset state, but also to draw focus on jenkins.io
    and prevent future misguidance of newbie contributors and GSoC students
    who want to contribute.

--- src/jio-navbar.ts
+++ src/jio-navbar.ts
@@ -178,11 +178,6 @@ export class Navbar extends LitElement {
           {label: msg("Conduct"), link: "/project/conduct/"},
           {label: msg("Artwork"), link: "/artwork/"},
         ]
-      },
-      {
-        label: msg("English"), link: [
-          {label: msg("中文 Chinese"), link: "/zh/"},
-        ]
       }
     ] as Array<NavbarItemLink>;
     const menuItemsHtml = menuItems.map((menuItem, idx) => {
