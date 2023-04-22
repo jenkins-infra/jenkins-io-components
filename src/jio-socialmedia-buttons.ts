b44eae4d5af426857424032bb847e4a49756a92e
Gavin Mogan <git@gavinmogan.com>
2023-04-22 10:51:08 -0700

    restore code after eslint-plugin-lit now properly detects left hand assigns

--- src/jio-socialmedia-buttons.ts
+++ src/jio-socialmedia-buttons.ts
@@ -19,10 +19,22 @@ export class SocialMediaButtons extends LitElement {
   linkedin = "";
 
   override render() {
-    const _twitter: TemplateResult | symbol = !this.twitter ? nothing : html`<li><a href=${`https://twitter.com/${this.twitter}`} target="_blank" rel="noreferrer noopener">Twitter</a></li>`;
-    const _github: TemplateResult | symbol = !this.github ? nothing : html`<li><a href=${`https://github.com/${this.github}`} target="_blank" rel="noreferrer noopener">Github</a></li>`;
-    const _blog: TemplateResult | symbol = !this.blog ? nothing : html`<li><a href=${this.blog} target="_blank" rel="noreferrer noopener">Blog</a></li>`;
-    const _linkedin: TemplateResult | symbol = !this.linkedin ? nothing : html`<li><a href=${`https://www.linkedin.com/in/${this.linkedin}`} target="_blank" rel="noreferrer noopener">LinkedIn</a></li>`;
+    let _twitter: TemplateResult | symbol = nothing;
+    if (this.twitter) {
+      _twitter = html`<li><a href=${`https://twitter.com/${this.twitter}`} target="_blank" rel="noreferrer noopener">Twitter</a></li>`;
+    }
+    let _github: TemplateResult | symbol = nothing;
+    if (this.github) {
+      _github = html`<li><a href=${`https://github.com/${this.github}`} target="_blank" rel="noreferrer noopener">Github</a></li>`;
+    }
+    let _blog: TemplateResult | symbol = nothing;
+    if (this.blog) {
+      _blog = html`<li><a href=${this.blog} target="_blank" rel="noreferrer noopener">Blog</a></li>`;
+    };
+    let _linkedin: TemplateResult | symbol = nothing;
+    if (this.linkedin) {
+      _linkedin = html`<li><a href=${`https://www.linkedin.com/in/${this.linkedin}`} target="_blank" rel="noreferrer noopener">LinkedIn</a></li>`;
+    }
     return html`
       <ul>
         ${_github}${_twitter}${_blog}${_linkedin}
