3aaeb3a56a2c3a92248445fed047b70bccd91a7c
Lakshmi Shree A <lakshmishreea122003@gmail.com>
2024-02-20 18:36:59 +0530

    Update jio-footer.ts

--- src/jio-footer.ts
+++ src/jio-footer.ts
@@ -63,7 +63,7 @@ export class Footer extends LitElement {
 <footer>
    <div class="container">
       <div class="row">
-         <div class="col-md-4">
+         <div class="col-md-4 col1">
             <p class="box">
                <jio-improve-this-page sourcePath=${this.sourcePath} githubRepo=${this.githubRepo} .githubBranch=${ifDefined(this.githubBranch)}></jio-improve-this-page>
                <jio-report-a-problem sourcePath=${this.sourcePath} githubRepo=${this.githubRepo} .githubBranch=${ifDefined(this.githubBranch)}></jio-report-a-problem>
