d3a58f87152d39c403f8188d414631ecdb4fab8b
Ratul <cs.ratul03@gmail.com>
2024-03-17 12:05:33 +0530

    add hover effect to Navbar

--- src/jio-navbar-link.ts
+++ src/jio-navbar-link.ts
@@ -13,6 +13,10 @@ export class NavbarLink extends LitElement {
   text-decoration: none;
   transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out;
 }
+.nav-link:hover{
+  text-decoration: underline;
+  color: rgba(255, 255, 255, 0.55);
+}
 .dropdown-item {
   background-color: transparent;
   border: 0;
